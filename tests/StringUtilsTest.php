c759ee51879a9baa7c76546fc9b1dbbddd9343f9
VoronczovaPolina <tirabyrius@gmail.com>
2023-05-10 16:50:49 +0500

    use package webmozart/assert

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,13 +1,11 @@
 <?php
 
-require("src/StringUtils.php");
+require_once __DIR__ . '/../vendor/autoload.php';
+use function StringUtils\capitalize;
 
-if (StringUtils\capitalize('hello') !== 'Hello') {
-    throw new \Exception('Функция работает неверно!');
-}
+use Webmozart\Assert\Assert;
 
-if (StringUtils\capitalize('') !== '') {
-    throw new \Exception('Функция работает неверно!');
-}
+Assert::eq(capitalize('hello'), 'Hello');
+Assert::eq(capitalize(''), '');
 
 echo 'Все тесты пройдены!';
